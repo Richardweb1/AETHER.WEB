d8a49126a16b894d519c4a0d5a643bfe5eabb623
Richardweb1 <adrdour1rachid@gmail.com>
2026-08-03 03:30:58 +0100

    Submit wallet transactions through Aptos testnet fullnode

--- components/wallet/WalletProvider.tsx
+++ components/wallet/WalletProvider.tsx
@@ -1,13 +1,27 @@
 "use client";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { Network } from "@aptos-labs/ts-sdk";
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+
+const APTOS_TESTNET_NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
+const aptosTestnet = new Aptos(
+  new AptosConfig({
+    network: Network.TESTNET,
+    fullnode: APTOS_TESTNET_NODE_URL,
+  })
+);
+
+const transactionSubmitter = {
+  async submitTransaction(args: Parameters<typeof aptosTestnet.transaction.submit.simple>[0]) {
+    return aptosTestnet.transaction.submit.simple(args);
+  },
+};
 
 export default function WalletProvider({ children }: { children: React.ReactNode }) {
   return (
     <AptosWalletAdapterProvider
       autoConnect={false}
       optInWallets={["Petra"]}
-      dappConfig={{ network: Network.TESTNET }}
+      dappConfig={{ network: Network.TESTNET, transactionSubmitter }}
     >
       {children}
     </AptosWalletAdapterProvider>
